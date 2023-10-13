619d3f6d8a9180df0d3c835baef811462e624c66
Sean McElroy <sean.mcelroy@sonitussystems.com>
2023-10-13 23:47:46 +0100

    Double progress made. Heartbeat sensor and spinning line

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 // Optional: You can add a function to start and stop the spinner.
 let spinnerAnimation;
 
-function startSpinner() {
-  spinnerAnimation = document.querySelector('.radar-spinner');
-  spinnerAnimation.style.display = 'block';
-}
+// function startSpinner() {
+//   spinnerAnimation = document.querySelector('.radar-spinner');
+//   spinnerAnimation.style.display = 'block';
+// }
 
-function stopSpinner() {
-  if (spinnerAnimation) {
-    spinnerAnimation.style.display = 'none';
-  }
-}
+// function stopSpinner() {
+//   if (spinnerAnimation) {
+//     spinnerAnimation.style.display = 'none';
+//   }
+// }
